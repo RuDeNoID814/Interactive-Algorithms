59cae751a45526bc81ee2f26d398da0a59d3fd95
Maksim <rudenko@rud-m.ru>
2026-09-15 11:34:07 +0500

    Initial commit

---
+++ AlgoLab/AlgoLab/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using AlgoLab;
+using ApexCharts;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddApexCharts();
+
+await builder.Build().RunAsync();
